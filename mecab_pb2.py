4ba66a44f46447bd2674694f1cf928171a8e161c
ayatoy <ayatoy@gmail.com>
2019-02-17 17:04:11 +0900

    Add some fields

--- mecab_pb2.py
+++ mecab_pb2.py
@@ -19,7 +19,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   package='mecab',
   syntax='proto3',
   serialized_options=None,
-  serialized_pb=_b('\n\x0bmecab.proto\x12\x05mecab\"!\n\x0cParseRequest\x12\x11\n\tsentences\x18\x01 \x03(\t\"3\n\rParseResponse\x12\"\n\tsentences\x18\x01 \x03(\x0b\x32\x0f.mecab.Sentence\"&\n\x08Sentence\x12\x1a\n\x05words\x18\x01 \x03(\x0b\x32\x0b.mecab.Word\"(\n\x04Word\x12\x0f\n\x07surface\x18\x01 \x01(\t\x12\x0f\n\x07\x66\x65\x61ture\x18\x02 \x03(\t2;\n\x05MeCab\x12\x32\n\x05Parse\x12\x13.mecab.ParseRequest\x1a\x14.mecab.ParseResponseb\x06proto3')
+  serialized_pb=_b('\n\x0bmecab.proto\x12\x05mecab\"O\n\x0cParseRequest\x12\x11\n\tsentences\x18\x01 \x03(\t\x12\x12\n\ndictionary\x18\x02 \x01(\t\x12\x0b\n\x03\x62os\x18\x03 \x01(\x08\x12\x0b\n\x03\x65os\x18\x04 \x01(\x08\"3\n\rParseResponse\x12\"\n\tsentences\x18\x01 \x03(\x0b\x32\x0f.mecab.Sentence\"&\n\x08Sentence\x12\x1a\n\x05words\x18\x01 \x03(\x0b\x32\x0b.mecab.Word\"(\n\x04Word\x12\x0f\n\x07surface\x18\x01 \x01(\t\x12\x0f\n\x07\x66\x65\x61ture\x18\x02 \x03(\t2;\n\x05MeCab\x12\x32\n\x05Parse\x12\x13.mecab.ParseRequest\x1a\x14.mecab.ParseResponseb\x06proto3')
 )
 
 
@@ -39,6 +39,27 @@ _PARSEREQUEST = _descriptor.Descriptor(
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='dictionary', full_name='mecab.ParseRequest.dictionary', index=1,
+      number=2, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=_b("").decode('utf-8'),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='bos', full_name='mecab.ParseRequest.bos', index=2,
+      number=3, type=8, cpp_type=7, label=1,
+      has_default_value=False, default_value=False,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='eos', full_name='mecab.ParseRequest.eos', index=3,
+      number=4, type=8, cpp_type=7, label=1,
+      has_default_value=False, default_value=False,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
   ],
   extensions=[
   ],
@@ -52,7 +73,7 @@ _PARSEREQUEST = _descriptor.Descriptor(
   oneofs=[
   ],
   serialized_start=22,
-  serialized_end=55,
+  serialized_end=101,
 )
 
 
@@ -82,8 +103,8 @@ _PARSERESPONSE = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=57,
-  serialized_end=108,
+  serialized_start=103,
+  serialized_end=154,
 )
 
 
@@ -113,8 +134,8 @@ _SENTENCE = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=110,
-  serialized_end=148,
+  serialized_start=156,
+  serialized_end=194,
 )
 
 
@@ -151,8 +172,8 @@ _WORD = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=150,
-  serialized_end=190,
+  serialized_start=196,
+  serialized_end=236,
 )
 
 _PARSERESPONSE.fields_by_name['sentences'].message_type = _SENTENCE
@@ -199,8 +220,8 @@ _MECAB = _descriptor.ServiceDescriptor(
   file=DESCRIPTOR,
   index=0,
   serialized_options=None,
-  serialized_start=192,
-  serialized_end=251,
+  serialized_start=238,
+  serialized_end=297,
   methods=[
   _descriptor.MethodDescriptor(
     name='Parse',
